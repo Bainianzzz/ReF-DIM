b235b246373df2448cdf7148f5069a5962e7ef78
Bainianzzz <3036349123@qq.com>
2025-09-21 14:08:50 +0800

    update DIM

--- model/DIM.py
+++ model/DIM.py
@@ -7,18 +7,19 @@ class Encoder(nn.Module):
 
         self.conv1 = Conv(c1=c1, c2=c_hidden, k=3, s=2)
         self.conv2 = Conv(c1=c_hidden, c2=c_hidden * 2, k=3, s=2)
+        self.conv3 = Conv(c1=c_hidden * 2, c2=c_hidden * 4, k=3, s=2)
 
         self.stage1 = C3k2(c1=c_hidden, c2=c_hidden, c3k=False, e=0.25)
         self.stage2 = C3k2(c1=c_hidden * 2, c2=c_hidden * 2, c3k=False, e=0.25)
 
-        self.attn1 = A2C2f(c1=c_hidden * 2, c2=c_hidden * 2, area=4, residual=True, e=0.5)
-        self.attn2 = A2C2f(c1=c_hidden * 2, c2=c_hidden * 2, area=1, residual=True, e=0.5)
+        self.attn1 = A2C2f(c1=c_hidden * 4, c2=c_hidden * 4, area=4, residual=True, e=0.25)
+        self.attn2 = A2C2f(c1=c_hidden * 4, c2=c_hidden * 4, area=1, residual=True, e=0.25)
 
     def forward(self, x):
         encoder_1 = self.stage1(self.conv1(x))
         encoder_2 = self.stage2(self.conv2(encoder_1))
 
-        attn = self.attn2(self.attn1(encoder_2))
+        attn = self.attn2(self.attn1(self.conv3(encoder_2)))
 
         return encoder_1, encoder_2, attn
 
@@ -30,6 +31,7 @@ class DIM(nn.Module):
 
         self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
 
+        self.conv0 = Conv(c1=c_hidden * 4, c2=c_hidden * 2)
         self.conv1 = Conv(c1=c_hidden * 4, c2=c_hidden)
         self.conv2 = Conv(c1=c_hidden * 2, c2=c_hidden)
         self.decoder = Conv(c1=c_hidden, c2=3, k=3)
@@ -37,6 +39,7 @@ class DIM(nn.Module):
     def forward(self, x):
         encoder_1, encoder_2, attn = self.encoder(x)
 
+        attn = self.conv0(self.upsample(attn))
         fusion_1 = torch.cat([attn, encoder_2], 1)
         IM_1 = self.intensity_mapping(fusion_1)
         IM_1 = self.conv1(IM_1)
@@ -59,7 +62,7 @@ class DIM(nn.Module):
 if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    input_tensor = torch.randn(8, 3, 256, 256).to(device)
+    input_tensor = torch.randn(1, 3, 800, 1200).to(device)
     model = DIM().to(device)
     output = model(input_tensor)
 
